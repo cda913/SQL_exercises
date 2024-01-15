6699314c4878a928c7e9dab3798cc39b18bf8828
cda913 <78565970+cda913@users.noreply.github.com>
2024-01-15 11:50:05 -0500

    Add files via upload

---
+++ CAitkin_SQLTasks_Tier_2.sql
@@ -0,0 +1,206 @@
+/* Welcome to the SQL mini project. You will carry out this project partly in
+the PHPMyAdmin interface, and partly in Jupyter via a Python connection.
+
+This is Tier 2 of the case study, which means that there'll be less guidance for you about how to setup
+your local SQLite connection in PART 2 of the case study. This will make the case study more challenging for you: 
+you might need to do some digging, aand revise the Working with Relational Databases in Python chapter in the previous resource.
+
+Otherwise, the questions in the case study are exactly the same as with Tier 1. 
+
+PART 1: PHPMyAdmin
+You will complete questions 1-9 below in the PHPMyAdmin interface. 
+Log in by pasting the following URL into your browser, and
+using the following Username and Password:
+
+URL: https://sql.springboard.com/
+Username: student
+Password: learn_sql@springboard
+
+The data you need is in the "country_club" database. This database
+contains 3 tables:
+    i) the "Bookings" table,
+    ii) the "Facilities" table, and
+    iii) the "Members" table.
+
+In this case study, you'll be asked a series of questions. You can
+solve them using the platform, but for the final deliverable,
+paste the code for each solution into this script, and upload it
+to your GitHub.
+
+Before starting with the questions, feel free to take your time,
+exploring the data, and getting acquainted with the 3 tables. */
+
+
+/* QUESTIONS 
+/* Q1: Some of the facilities charge a fee to members, but some do not.
+Write a SQL query to produce a list of the names of the facilities that do. */
+SELECT * FROM Facilities WHERE membercost > 0
+Tennis Court 1
+Tennis Court 2
+Massage Room 1
+Massage Room 2
+Squash Court
+
+/* Q2: How many facilities do not charge a fee to members? */
+SELECT COUNT(name) FROM Facilities WHERE membercost= 0
+4
+
+/* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
+where the fee is less than 20% of the facility's monthly maintenance cost.
+Return the facid, facility name, member cost, and monthly maintenance of the
+facilities in question. */
+SELECT facid, name, membercost, monthlymaintenance 
+FROM Facilities 
+WHERE (membercost < 0.2*monthlymaintenance) AND (membercost > 0)
+0	Tennis Court 1	5	200
+1	Tennis Court 2	5	200
+4	Massage Room 1	9.9	3000
+5	Massage Room 2	9.9	3000
+6	Squash Court	3.5	80
+
+
+/* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
+Try writing the query without using the OR operator. */
+SELECT *
+FROM Facilities
+WHERE facid IN (1,5)
+1	Tennis Court 2	5	25	8000	200
+5	Massage Room 2	9.9	80	4000	3000
+
+/* Q5: Produce a list of facilities, with each labelled as
+'cheap' or 'expensive', depending on if their monthly maintenance cost is
+more than $100. Return the name and monthly maintenance of the facilities
+in question. */
+SELECT name, monthlymaintenance,
+	CASE 
+		WHEN monthlymaintenance > 100 THEN 'Expensive'
+		WHEN monthlymaintenance <= 100 THEN 'Cheap'
+	END AS rating
+FROM  Facilities 
+Tennis Court 1	200	Expensive
+Tennis Court 2	200	Expensive
+Badminton Court	50	Cheap
+Table Tennis	10	Cheap
+Massage Room 1	3000	Expensive
+Massage Room 2	3000	Expensive
+Squash Court	80	Cheap
+Snooker Table	15	Cheap
+Pool Table	15	Cheap
+
+
+/* Q6: You'd like to get the first and last name of the last member(s)
+who signed up. Try not to use the LIMIT clause for your solution. */
+SELECT firstname, surname, Max(joindate)
+FROM Members
+Darren	Smith	2012-09-26 18:08:45
+
+/* Q7: Produce a list of all members who have used a tennis court.
+Include in your output the name of the court, and the name of the member
+formatted as a single column. Ensure no duplicate data, and order by
+the member name. */
+SELECT DISTINCT(firstname || ' ' || surname) AS mem_name, name
+FROM Members
+INNER JOIN Bookings ON Members.memid = Bookings.memid
+INNER JOIN Facilities ON Bookings.facid = Facilities.facid
+WHERE Facilities.facid IN (0,1)
+ORDER BY surname, firstname, name
+Florence Bader	Tennis Court 1
+Florence Bader	Tennis Court 2
+Anne Baker	Tennis Court 1
+Anne Baker	Tennis Court 2
+Timothy Baker	Tennis Court 1
+Timothy Baker	Tennis Court 2
+Tim Boothe	Tennis Court 1
+Tim Boothe	Tennis Court 2
+Gerald Butters	Tennis Court 1
+Gerald Butters	Tennis Court 2
+Joan Coplin	Tennis Court 1
+Erica Crumpet	Tennis Court 1
+Nancy Dare	Tennis Court 1
+Nancy Dare	Tennis Court 2
+David Farrell	Tennis Court 1
+David Farrell	Tennis Court 2
+Jemima Farrell	Tennis Court 1
+Jemima Farrell	Tennis Court 2
+GUEST GUEST	Tennis Court 1
+GUEST GUEST	Tennis Court 2
+Matthew Genting	Tennis Court 1
+John Hunt	Tennis Court 1
+John Hunt	Tennis Court 2
+David Jones	Tennis Court 1
+David Jones	Tennis Court 2
+Douglas Jones	Tennis Court 1
+Janice Joplette	Tennis Court 1
+Janice Joplette	Tennis Court 2
+Charles Owen	Tennis Court 1
+Charles Owen	Tennis Court 2
+David Pinker	Tennis Court 1
+Millicent Purview	Tennis Court 2
+Tim Rownam	Tennis Court 1
+Tim Rownam	Tennis Court 2
+Henrietta Rumney	Tennis Court 2
+Ramnaresh Sarwin	Tennis Court 1
+Ramnaresh Sarwin	Tennis Court 2
+Darren Smith	Tennis Court 2
+Jack Smith	Tennis Court 1
+Jack Smith	Tennis Court 2
+Tracy Smith	Tennis Court 1
+Tracy Smith	Tennis Court 2
+Ponder Stibbons	Tennis Court 1
+Ponder Stibbons	Tennis Court 2
+Burton Tracy	Tennis Court 1
+Burton Tracy	Tennis Court 2
+
+
+/* Q8: Produce a list of bookings on the day of 2012-09-14 which
+will cost the member (or guest) more than $30. Remember that guests have
+different costs to members (the listed costs are per half-hour 'slot'), and
+the guest user's ID is always 0. Include in your output the name of the
+facility, the name of the member formatted as a single column, and the cost.
+Order by descending cost, and do not use any subqueries. */
+SELECT 
+	firstname || ' ' || surname AS mem_name,
+	Facilities.name,
+	(CASE 
+		WHEN Bookings.memid = 0 then guestcost * slots
+		WHEN Bookings.memid <> 0 then membercost  * slots
+	END) AS cost 
+FROM Members
+INNER JOIN Bookings ON Bookings.memid = Members.memid
+INNER JOIN Facilities ON Bookings.facid = Facilities.facid
+WHERE (DATE(starttime) = '2012-09-14') AND cost > 30
+ORDER BY cost DESC
+GUEST GUEST	Massage Room 2	320
+GUEST GUEST	Massage Room 1	160
+GUEST GUEST	Massage Room 1	160
+GUEST GUEST	Massage Room 1	160
+GUEST GUEST	Tennis Court 2	150
+GUEST GUEST	Tennis Court 1	75
+GUEST GUEST	Tennis Court 1	75
+GUEST GUEST	Tennis Court 2	75
+GUEST GUEST	Squash Court	70.0
+Jemima Farrell	Massage Room 1	39.6
+GUEST GUEST	Squash Court	35.0
+GUEST GUEST	Squash Court	35.0
+
+/* Q9: This time, produce the same result as in Q8, but using a subquery. */
+
+
+/* PART 2: SQLite
+
+Export the country club data from PHPMyAdmin, and connect to a local SQLite instance from Jupyter notebook 
+for the following questions.  
+
+QUESTIONS:
+/* Q10: Produce a list of facilities with a total revenue less than 1000.
+The output of facility name and total revenue, sorted by revenue. Remember
+that there's a different cost for guests and members! */
+
+/* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
+
+
+/* Q12: Find the facilities with their usage by member, but not guests */
+
+
+/* Q13: Find the facilities usage by month, but not guests */
+
